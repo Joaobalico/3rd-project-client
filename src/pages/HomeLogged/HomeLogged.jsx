6952aa70d9bf473515b7490e23fa74bdf0cf3ca4
Joao Balico <joaobalico12@hotmail.com>
2022-03-18 06:10:03 +0000

    Add API

--- src/pages/HomeLogged/HomeLogged.jsx
+++ src/pages/HomeLogged/HomeLogged.jsx
@@ -6,6 +6,7 @@ import { AuthContext } from "../../context/auth.context";
 const HomeLogged = () => {
   const { user } = useContext(AuthContext);
   const [events, setEvents] = useState([]);
+  const [quotes, setQuotes] = useState(null);
 
   const storedToken = localStorage.getItem("authToken");
 
@@ -29,16 +30,33 @@ const HomeLogged = () => {
     fetchEvents();
   }, []);
 
+  const fetchQuotes = async () => {
+    try {
+      let response = await axios.get("https://type.fit/api/quotes");
+      let quotesFromApi = response.data;
+      console.log(quotesFromApi);
+      let randomQuote =
+        quotesFromApi[Math.floor(Math.random() * quotesFromApi.length)];
+      setQuotes(randomQuote);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
   return (
     <>
       <h1>Welcome, {user.username} 👋</h1>
       <h2>
         <i>All Events:</i>
       </h2>
-      <div className="list" style={{display: 'flex', flexWrap:"wrap"}}>
+      <div className="list" style={{ display: "flex", flexWrap: "wrap" }}>
         {events.map((event) => {
           return (
-            <div key={event._id} className="card" style={{margin:"1rem"}}>
+            <div key={event._id} className="card" style={{ margin: "1rem" }}>
               <div className="card-body">
                 <Link to={`/event/${event._id}`} style={{ color: "black" }}>
                   <h3 className="card-title">{event.title}</h3>
@@ -54,6 +72,15 @@ const HomeLogged = () => {
           );
         })}
       </div>
+      {quotes && (
+        <div className="quote-box">
+      <hr />
+          <div className="quote">
+            <h4>{quotes.author}</h4> <br />
+            <p>{quotes.text}</p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
